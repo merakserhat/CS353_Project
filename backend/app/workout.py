9af9d8e68caede266ff7ca86fbd93aaf365ce42f
Ege <egekaraahmetoglu2003@gmail.com>
2024-05-18 05:47:53 +0300

    feat: workout create fe

--- backend/app/workout.py
+++ backend/app/workout.py
@@ -1,2 +1,27 @@
-from Flask import Flask, request, jsonify
+from Flask import Blueprint, request, jsonify
 import MySQLdb.cursors
+from database import connect
+
+workout = Blueprint('workout', __name__, url_prefix='/workout')
+
+@workout.route('/create/session/fe', methods=['POST'])
+def create_session_fe():
+    connection = connect()
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM FitnessEnthusiast WHERE fe_id = %s', (request.json['fe_id'],))
+    fe = cursor.fetchone()
+    if fe is None:
+        return jsonify({'message': 'Fitness enthusiast not found!'})
+    
+    fe_id = request.json['fe_id']
+    trainer_id = request.json['trainer_id']
+    name = request.json['name']
+    audience = request.json['audience']
+    description = request.json['description']
+    duration = request.json['duration']
+    availability = request.json['availability']
+    cursor.execute('INSERT INTO WorkoutSession(fe_id, trainer_id, name, audience, description, duration, availability) VALUES (%s, %s, %s, %s, %s, %s)', (fe_id, trainer_id, name, audience, description, duration, availability))
+    connection.commit()
+    cursor.close()
+
+    return jsonify({'message': 'FE Workout Session created successfully!'})
