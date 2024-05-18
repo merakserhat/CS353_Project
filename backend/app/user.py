92b303100b64cb3d38059fb201383aae42c9eb4a
Orhun Aysan <91375826+aysanorhun@users.noreply.github.com>
2024-05-18 04:01:54 +0300

    Add files via upload

---
+++ backend/app/user.py
@@ -0,0 +1,104 @@
+from flask import Blueprint, request, jsonify
+import MySQLdb.cursors
+from database import connect
+import uuid
+
+user = Blueprint('user', __name__, url_prefix='/user')
+
+@user.route('/login/fe', methods=['POST'])
+def login_fe():
+    email = request.json['email']
+    password = request.json['password']
+
+    connection = connect()
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM User WHERE email = %s AND password = %s', (email, password))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Invalid credentials!'})
+    cursor.execute('SELECT * FROM FitnessEnthusiast WHERE fe_id = %s', (user['user_id'],))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Invalid credentials!'})
+    cursor.close()
+
+    return jsonify({'message': 'Login successful!', 'user': user})
+
+@user.route('/login/trainer', methods=['POST'])
+def login_trainer():
+    email = request.json['email']
+    password = request.json['password']
+
+    connection = connect()
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM User WHERE email = %s AND password = %s', (email, password))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Invalid credentials!'})
+    cursor.execute('SELECT * FROM Trainer WHERE trainer_id = %s', (user['user_id'],))
+    user = cursor.fetchone()
+    if user is None:
+        return jsonify({'message': 'Invalid credentials!'})
+    cursor.close()
+
+    return jsonify({'message': 'Login successful!', 'user': user})
+
+@user.route('/register/fe', methods=['POST'])
+def register_fe():
+    user_id = 1
+    email = request.json['email']
+    password = request.json['password']
+    first_name = request.json['first_name']
+    middle_name = request.json['middle_name']
+    last_name = request.json['last_name']
+    
+    fe_id = user_id
+    weight = request.json['weight']
+    height = request.json['height']
+    age = request.json['age']
+    gender = request.json['gender']
+
+    # Check if user already exists
+    connection = connect()
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM User WHERE user_id = %s', (user_id,))
+    if cursor.fetchone() is not None:
+        return jsonify({'message': 'User already exists!'})
+
+    # Insert user and fitness enthusiast data
+    cursor.execute('INSERT INTO User (user_id, email, password, first_name, middle_name, last_name) VALUES (%s, %s, %s, %s, %s, %s)', (user_id, email, password, first_name, middle_name, last_name))
+    cursor.execute('INSERT INTO FitnessEnthusiast (fe_id, weight, height, age, gender) VALUES (%s, %s, %s, %s, %s)', (fe_id, weight, height, age, gender))
+    connection.commit()
+    cursor.close()
+    return jsonify({'message': 'User registered successfully!'})
+
+@user.route('/register/trainer', methods=['POST'])
+def register_trainer():
+    user_id = uuid.uuid4().int >> 64
+    email = request.json['email']
+    password = request.json['password']
+    first_name = request.json['first_name']
+    middle_name = request.json['middle_name']
+    last_name = request.json['last_name']
+    
+    trainer_id = user_id
+    gender = request.json['gender']
+    fee = request.json['fee']
+    description = request.json['description']
+    specialization = request.json['specialization']
+    experience = request.json['experience']
+    ratings = 0
+
+    # Check if user already exists
+    connection = connect()
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM User WHERE user_id = %s', (user_id,))
+    if cursor.fetchone() is not None:
+        return jsonify({'message': 'User already exists!'})
+
+    # Insert user and trainer data
+    cursor.execute('INSERT INTO User (user_id, email, password, first_name, middle_name, last_name) VALUES (%s, %s, %s, %s, %s, %s)', (user_id, email, password, first_name, middle_name, last_name))
+    cursor.execute('INSERT INTO Trainer (trainer_id, gender, fee, description, specialization, experience, ratings) VALUES (%s, %s, %s, %s, %s, %s, %s)', (trainer_id, gender, fee, description, specialization, experience, ratings))
+    connection.commit()
+    cursor.close()
+    return jsonify({'message': 'Trainer registered successfully!'})
---
+++ backend/schema.sql
@@ -0,0 +1,450 @@
+CREATE DATABASE IF NOT EXISTS fitnesstrackerdb;
+
+USE fitnesstrackerdb;
+
+CREATE TABLE User (
+    user_id INT NOT NULL,
+    email VARCHAR(255),
+    password VARCHAR(40),
+    first_name VARCHAR(255),
+    middle_name VARCHAR(255),
+    last_name VARCHAR(255),
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE Trainer (
+    trainer_id INT NOT NULL,
+    gender VARCHAR(20),
+    fee INT,
+    description VARCHAR(255),
+    specialization VARCHAR(20),
+    experience VARCHAR(255),
+    ratings INT,
+    PRIMARY KEY(trainer_id),
+    FOREIGN KEY(trainer_id) references User(user_id)
+);
+
+CREATE TABLE Admin (
+    admin_id INT NOT NULL,
+    PRIMARY KEY(admin_id),
+    FOREIGN KEY(admin_id) references User(user_id)
+);
+
+CREATE TABLE FitnessEnthusiast (
+    fe_id INT NOT NULL,
+    weight INT,
+    height INT,
+    age INT,
+    gender VARCHAR(20),
+    PRIMARY KEY(fe_id),
+    FOREIGN KEY(fe_id) references User(user_id)
+);
+
+CREATE TABLE Payment (
+    payment_id INT NOT NULL,
+    amount INT NOT NULL,
+    date_time VARCHAR(50),
+    fe_id INT NOT NULL,
+    trainer_id INT NOT NULL,
+    PRIMARY KEY(payment_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id),
+    FOREIGN KEY(trainer_id) references Trainer(trainer_id)
+);
+
+CREATE TABLE FitnessGoal (
+    goal_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    type VARCHAR(20),
+    value VARCHAR(20),
+    time_interval INT,
+    PRIMARY KEY(goal_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE PastAchievement (
+    ach_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    goal_id INT NOT NULL,
+    PRIMARY KEY(ach_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id),
+    FOREIGN KEY(goal_id) references FitnessGoal(goal_id)
+);
+
+CREATE TABLE Exercise (
+    exercise_id INT NOT NULL,
+    target_region VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY(exercise_id)
+);
+
+CREATE TABLE Challenge (
+    challenge_id INT NOT NULL,
+    name VARCHAR(255),
+    prize VARCHAR(255),
+    start_date INT,
+    end_date INT,
+    exercise_id INT NOT NULL,
+    PRIMARY KEY(challenge_id),
+    FOREIGN KEY(exercise_id) references Exercise(exercise_id)
+);
+
+CREATE TABLE Recommendation (
+    recommendation_id INT NOT NULL,
+    message VARCHAR(255),
+    PRIMARY KEY(recommendation_id)
+);
+
+CREATE TABLE TrainerSession (
+    session_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    trainer_id INT NOT NULL,
+    start_time INT,
+    end_time INT,
+    PRIMARY KEY(session_id, fe_id, trainer_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id),
+    FOREIGN KEY(trainer_id) references Trainer(trainer_id)
+);
+
+CREATE TABLE Chat (
+    chat_id INT NOT NULL,
+    session_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    trainer_id INT NOT NULL,
+    start_date INT,
+    PRIMARY KEY(chat_id, session_id, fe_id, trainer_id),
+    FOREIGN KEY(session_id, fe_id, trainer_id) references TrainerSession(session_id, fe_id, trainer_id)
+);
+
+CREATE TABLE Message (
+    message_id INT NOT NULL,
+    chat_id INT NOT NULL,
+    session_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    trainer_id INT NOT NULL,
+    content VARCHAR(255),
+    date_time INT,
+    owner VARCHAR(255),
+    PRIMARY KEY(
+        message_id,
+        chat_id,
+        session_id,
+        fe_id,
+        trainer_id
+    ),
+    FOREIGN KEY(chat_id, session_id, fe_id, trainer_id) references Chat(chat_id, session_id, fe_id, trainer_id)
+);
+
+CREATE TABLE NutritionPlan (
+    plan_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    name VARCHAR(50),
+    content VARCHAR(255),
+    PRIMARY KEY(plan_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE NutritionLog (
+    nutlog_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    date_time INT,
+    PRIMARY KEY(nutlog_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE Nutrition (
+    nut_id INT,
+    name VARCHAR(50),
+    calorie INT,
+    protein INT,
+    PRIMARY KEY(nut_id)
+);
+
+CREATE TABLE ExerciseLog (
+    exlog_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    date_time INT,
+    PRIMARY KEY(exlog_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE WorkoutLog (
+    workoutlog_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    date_time INT,
+    PRIMARY KEY(workoutlog_id, fe_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE WorkoutSession (
+    workout_id INT NOT NULL,
+    fe_id INT,
+    trainer_id INT,
+    name VARCHAR(50),
+    audience VARCHAR(20),
+    description VARCHAR(255),
+    duration INT,
+    availability INT,
+    PRIMARY KEY(workout_id, fe_id, trainer_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id),
+    FOREIGN KEY(trainer_id) references Trainer(trainer_id)
+);
+
+CREATE TABLE Equipment (
+    workout_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    trainer_id INT NOT NULL,
+    equipment VARCHAR(50),
+    PRIMARY KEY(workout_id, fe_id, trainer_id, equipment),
+    FOREIGN KEY(workout_id, fe_id, trainer_id) references WorkoutSession(workout_id, fe_id, trainer_id)
+);
+
+CREATE TABLE keeps_workout (
+    workout_id INT NOT NULL,
+    fe_id INT,
+    trainer_id INT,
+    workoutlog_id INT NOT NULL,
+    PRIMARY KEY(workout_id, fe_id, trainer_id, workoutlog_id),
+    FOREIGN KEY(workout_id, fe_id, trainer_id) references WorkoutSession(workout_id, fe_id, trainer_id),
+    FOREIGN KEY(workoutlog_id, fe_id) references WorkoutLog(workoutlog_id, fe_id)
+);
+
+CREATE TABLE consists_of_exercise (
+    workout_id INT,
+    fe_id INT,
+    trainer_id INT,
+    exercise_id INT,
+    PRIMARY KEY(workout_id, fe_id, trainer_id, exercise_id),
+    FOREIGN KEY(workout_id, fe_id, trainer_id) references WorkoutSession(workout_id, fe_id, trainer_id),
+    FOREIGN KEY(exercise_id) references Exercise(exercise_id)
+);
+
+CREATE TABLE keeps_exercise (
+    exlog_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    exercise_id INT NOT NULL,
+    set_count INT,
+    repetition INT,
+    PRIMARY KEY(exlog_id, fe_id, exercise_id),
+    FOREIGN KEY(exlog_id, fe_id) references ExerciseLog(exlog_id, fe_id),
+    FOREIGN KEY(exercise_id) references Exercise(exercise_id)
+);
+
+CREATE TABLE recieves_recom (
+    recommendation_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    PRIMARY KEY(recommendation_id, fe_id),
+    FOREIGN KEY(recommendation_id) references Recommendation(recommendation_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id)
+);
+
+CREATE TABLE keeps_nutrition (
+    nut_id INT NOT NULL,
+    nutlog_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    portion INT,
+    PRIMARY KEY(nut_id, nutlog_id, fe_id),
+    FOREIGN KEY(nut_id) references Nutrition(nut_id),
+    FOREIGN KEY(nutlog_id, fe_id) references NutritionLog(nutlog_id, fe_id)
+);
+
+CREATE TABLE enters_challenge (
+    fe_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    PRIMARY KEY(fe_id, challenge_id),
+    FOREIGN KEY(fe_id) references FitnessEnthusiast(fe_id),
+    FOREIGN KEY(challenge_id) references Challenge(challenge_id)
+);
+
+
+CREATE TABLE consists_of_nut (
+    nut_id INT NOT NULL,
+    plan_id INT NOT NULL,
+    fe_id INT NOT NULL,
+    portion INT,
+    PRIMARY KEY(nut_id, plan_id, fe_id),
+    FOREIGN KEY(nut_id) references Nutrition(nut_id),
+    FOREIGN KEY(plan_id, fe_id) references NutritionPlan(plan_id, fe_id)
+);
+
+-- -- Inserting values into the User table
+-- INSERT INTO
+--     User (user_id, email, password, first_name, last_name)
+-- VALUES
+--     (
+--         1,
+--         'orhunaysan3b@gmail.com',
+--         'password1',
+--         'orhun',
+--         'aysan'
+--     ),
+--     (
+--         2,
+--         'egekaraahmetoglu2003@gmail.com',
+--         'password2',
+--         'ege',
+--         'karaahmetoglu'
+--     ),
+--     (
+--         3,
+--         'serhatmerak@gmail.com',
+--         'password3',
+--         'serhat',
+--         'merak'
+--     ),
+--     (
+--         4,
+--         'mirzaatalar@gmail.com',
+--         'password4',
+--         'mirza',
+--         'atalar'
+--     ),
+--     (
+--         5,
+--         'kaanturkoglu@gmail.com',
+--         'password5',
+--         'kaan',
+--         'turkoglu'
+--     );
+    
+-- -- Inserting values into the Trainer table
+-- INSERT INTO
+--     Trainer (
+--         trainer_id,
+--         gender,
+--         fee,
+--         description,
+--         specialization,
+--         experience,
+--         ratings
+--     )
+-- VALUES
+--     (
+--         1,
+--         'Male',
+--         50,
+--         'Certified personal trainer with 5 years of experience',
+--         'Weightlifting',
+--         '5 years',
+--         4
+--     ),
+--     (
+--         2,
+--         'Male',
+--         60,
+--         'Experienced yoga instructor specializing in Hatha yoga',
+--         'Yoga',
+--         '8 years',
+--         5
+--     );
+
+-- -- Inserting values into the FitnessEnthusiast table
+-- INSERT INTO
+--     FitnessEnthusiast (fe_id, weight, height, age, gender)
+-- VALUES
+--     (3, 70, 175, 30, 'Male'),
+--     (4, 55, 160, 25, 'Female');
+
+-- INSERT INTO Admin (admin_id) 
+-- VALUES(5);
+
+-- -- Inserting values into the Payment table
+-- INSERT INTO
+--     Payment (payment_id, amount, date_time, fe_id, trainer_id)
+-- VALUES
+--     (1, 50, '2024-05-13', 3, 1),
+--     (2, 60, '2024-05-13', 4, 2);
+
+-- INSERT INTO FitnessGoal (goal_id, fe_id, type, value, time_interval) 
+-- VALUES 
+-- (1, 3, 'Weight Loss', '10 kg', 3),
+-- (2, 4, 'Muscle Gain', '5 kg', 6);
+
+-- -- Inserting values into the PastAchievement table
+-- INSERT INTO PastAchievement (ach_id, fe_id, goal_id) 
+-- VALUES 
+-- (1, 3, 1),
+-- (2, 4, 2);
+
+-- -- Inserting values into the Challenge table
+-- INSERT INTO Challenge (challenge_id, name, prize, start_date, end_date, exercise_id) 
+-- VALUES 
+-- (1, 'Summer Fitness Challenge', 'Free gym membership', '2024-06-01', '2024-08-31', 1),
+-- (2, 'Yoga Wellness Challenge', 'Wellness package', '2024-07-01', '2024-07-31', 2);
+
+-- -- Inserting values into the Recommendation table
+-- INSERT INTO Recommendation (recommendation_id, message) 
+-- VALUES 
+-- (1, 'Try incorporating more protein into your diet'),
+-- (2, 'Consider adding strength training to your workout routine');
+
+-- -- Inserting values into the TrainerSession table
+-- INSERT INTO TrainerSession (session_id, fe_id, trainer_id, start_time, end_time) 
+-- VALUES 
+-- (1, 3, 1, '2024-05-13 10:00:00', '2024-05-13 11:00:00'),
+-- (2, 4, 2, '2024-05-14 15:00:00', '2024-05-14 16:00:00');
+
+-- -- Inserting values into the Chat table
+-- INSERT INTO Chat (chat_id, session_id, fe_id, trainer_id, start_date) 
+-- VALUES 
+-- (1, 1, 3, 1, '2024-05-13'),
+-- (2, 2, 4, 2, '2024-05-14');
+
+-- -- Inserting values into the Message table
+-- INSERT INTO Message (message_id, chat_id, session_id, fe_id, trainer_id, content, date_time, owner) 
+-- VALUES 
+-- (1, 1, 1, 3, 1, 'Hi, how can I help you today?', '2024-05-13 10:05:00', 'Trainer'),
+-- (2, 1, 1, 3, 1, 'I have some questions about my workout plan', '2024-05-13 10:10:00', 'Fitness Enthusiast');
+
+-- -- Inserting values into the NutritionPlan table
+-- INSERT INTO NutritionPlan (plan_id, fe_id, name, content) 
+-- VALUES 
+-- (1, 3, 'Muscle Gain Plan', 'High-protein diet with moderate carbs and fats'),
+-- (2, 4, 'Weight Loss Plan', 'Balanced diet with calorie deficit');
+
+-- -- Inserting values into the NutritionLog table
+-- INSERT INTO NutritionLog (nutlog_id, fe_id, date_time) 
+-- VALUES 
+-- (1, 3, '2024-05-13'),
+-- (2, 4, '2024-05-14');
+
+-- -- Inserting values into the Nutrition table
+-- INSERT INTO Nutrition (nut_id, name, calorie, protein) 
+-- VALUES 
+-- (1, 'Chicken Breast', 165, 31),
+-- (2, 'Brown Rice', 216, 5);
+
+-- -- Inserting values into the ExerciseLog table
+-- INSERT INTO ExerciseLog (exlog_id, fe_id, date_time) 
+-- VALUES 
+-- (1, 3, '2024-05-13'),
+-- (2, 4, '2024-05-14');
+
+-- -- Inserting values into the Exercise table
+-- INSERT INTO Exercise (exercise_id, target_region, description) 
+-- VALUES 
+-- (1, 'Upper Body', 'Bench Press'),
+-- (2, 'Flexibility', 'Forward Fold');
+
+-- -- Inserting values into the WorkoutLog table
+-- INSERT INTO WorkoutLog (workoutlog_id, fe_id, date_time) 
+-- VALUES 
+-- (1, 3, '2024-05-13'),
+-- (2, 4, '2024-05-14');
+
+-- -- Inserting values into the WorkoutSession table
+-- INSERT INTO WorkoutSession (workout_id, fe_id, trainer_id, name, audience, description, duration, availability) 
+-- VALUES 
+-- (1, 3, 1, 'Strength Training', 'Beginner', 'Introduction to basic strength training exercises', 60, 1),
+-- (2, 4, 2, 'Yoga Flow', 'Intermediate', 'Dynamic yoga flow sequence for flexibility and strength', 75, 1);
+
+-- -- Inserting values into the Equipment table
+-- INSERT INTO Equipment (workout_id, fe_id, trainer_id, equipment) 
+-- VALUES 
+-- (1, 3, 1, 'Barbell'),
+-- (2, 4, 2, 'Yoga Mat');
+
+-- -- Inserting values into the keeps_workout table
+-- INSERT INTO keeps_workout (workout_id, fe_id, trainer_id, workoutlog_id) 
+-- VALUES 
+-- (1, 3, 1, 1),
+-- (2, 4, 2, 2);
