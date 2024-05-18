1fa10543335ec38e074e3c6ad4bcabe755718747
Kaan Türkoğlu <kaanturkoglu2003@hotmail.com>
2024-05-18 23:42:02 +0300

    feat: excercise insertion

--- backend/app/exercise.py
+++ backend/app/exercise.py
@@ -13,37 +13,36 @@ def exercise_list():
     exercises = cursor.fetchall()
     if not exercises:
         return 'No exercises in the system'
-    for exercise in exercises:
-            exercise['exercise_id'] = exercise['exercise_id'].hex()
+    
 
     connection.commit()
     cursor.close()
 
     return jsonify(exercises)
 
-@exercise.route('/create', methods=['POST'])
-def create_exercise():
-    data = request.get_json()
+# @exercise.route('/create', methods=['POST'])
+# def create_exercise():
+#     data = request.get_json()
 
-    if not data or not all(k in data for k in ("exercise_name", "target_region", "description")):
-        return jsonify({"message": "Invalid input"}), 400
+#     if not data or not all(k in data for k in ("exercise_name", "target_region", "description")):
+#         return jsonify({"message": "Invalid input"}), 400
 
-    exercise_id = 1
-    exercise_name = data['exercise_name']
-    target_region = data['target_region']
-    description = data['description']
+#     exercise_id = "2"
+#     exercise_name = data['exercise_name']
+#     target_region = data['target_region']
+#     description = data['description']
 
-    connection = connect()
-    cursor = connection.cursor()
+#     connection = connect()
+#     cursor = connection.cursor()
 
-    query = """
-    INSERT INTO Exercise (exercise_id, exercise_name, target_region, description)
-    VALUES (%s, %s, %s, %s)
-    """
-    cursor.execute(query, (exercise_id, exercise_name, target_region, description))
-    connection.commit()
+#     query = """
+#     INSERT INTO Exercise (exercise_id, exercise_name, target_region, description)
+#     VALUES (%s, %s, %s, %s)
+#     """
+#     cursor.execute(query, (exercise_id, exercise_name, target_region, description))
+#     connection.commit()
 
-    cursor.close()
-    connection.close()
+#     cursor.close()
+#     connection.close()
 
-    return jsonify({"message": "Exercise created successfully", "exercise_id": exercise_id.hex()}), 201
+#     return jsonify({"message": "Exercise created successfully", "exercise_id": exercise_id.hex()}), 201
