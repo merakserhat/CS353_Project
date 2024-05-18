15e6db291f0a339eaf8fb573e676772ad9673dc9
aysanorhun <orhunaysan3@gmail.com>
2024-05-18 21:25:22 +0300

    feat: added blueprints

--- backend/app/app.py
+++ backend/app/app.py
@@ -8,13 +8,18 @@ from database import db
 from user import user
 from exercise import exercise
 from exercise_log import exercise_log
+from workout import workout
+from fitness_goal import fitness_goal
+from achievement import achievement
 
 CORS(db)
 
 db.register_blueprint(user)
 db.register_blueprint(exercise)
 db.register_blueprint(exercise_log)
-
+db.register_blueprint(workout)
+db.register_blueprint(fitness_goal)
+db.register_blueprint(achievement)
 
 @db.route('/')
 
